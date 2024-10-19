9cbbf9165905f28b608b7a67125658ac9d1d1947
Juan Arguello <arguellojuan08@gmail.com>
2024-10-18 22:13:22 -0300

    first winerie

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import WinerySelector from './components/WinerySelector/WinerySelector'
+const App = () => {
+    return (
+      <>
+          <WinerySelector />
+      </>
+    )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import UniSwap from './components/UniSwap';
+// import React from 'react'
+// import UniSwap from './components/UniSwap';
 
-// Aseguramos que el Custom Element se registre solo una vez
-if (!customElements.get('uniswap-enchainte')) {
-  window.customElements.define('uniswap-enchainte', UniSwap);
-}
+// // Aseguramos que el Custom Element se registre solo una vez
+// if (!customElements.get('uniswap-enchainte')) {
+//   window.customElements.define('uniswap-enchainte', UniSwap);
+// }
 
-// Creamos una instancia segura del root
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import WinerySelector from './components/WinerySelector/WinerySelector';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <App />
+  </React.StrictMode>
+);
